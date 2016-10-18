620448073c5bf0a320bc3bd7664f51400d593644
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-10-18 20:01:18 +0200

    GENERATED scaffold for task model + MIGRATED

---
+++ db/migrate/20161018175958_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name,         null: false
+      t.string :description
+      t.string :task_type
+
+      t.timestamps
+    end
+  end
+end
