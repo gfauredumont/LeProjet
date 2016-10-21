ea3ce9ed581748f7dcaf40fd2a675b0825138c04
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-10-21 19:41:43 +0200

    CREATED scope 'unassigned' for task model IMPLEMENTED after creating feature test

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
 
   has_many  :projects_tasks
   has_many  :projects, through: :projects_tasks
+
+  scope :unassigned, -> { Task.includes(:projects).where( :projects => { :id => nil } ) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,8 +4,23 @@ RSpec.describe Task, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:task)).to be_valid
   end
-  it { should validate_presence_of(:name) }
+  it { expect validate_presence_of(:name) }
 
-  it { should have_many(:projects) }
+  it { expect have_many(:projects) }
 
+  it "is unassigned when freshly created" do
+    task = FactoryGirl.create(:task)
+
+    expect(Task.unassigned).to include(task)
+    # Task.unassigned.should include(task)
+  end
+  it "is NOT unassigned when added to a project" do
+    project = FactoryGirl.create(:project)
+
+    task = FactoryGirl.create(:task)
+    task.projects << project
+
+    expect(Task.unassigned).to_not include(task)
+    # Task.unassigned.should_not include(task)
+  end
 end
