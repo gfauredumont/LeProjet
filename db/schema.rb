33e90487cb89e3d42e63daa87c6cf53653896567
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-10-28 18:50:32 +0200

    ADDED mandatory owner_id property to project table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027141824) do
+ActiveRecord::Schema.define(version: 20161028164303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161027141824) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "owner_id",    null: false
   end
 
   create_table "projects_tasks", force: :cascade do |t|
