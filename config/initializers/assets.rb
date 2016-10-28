e241927064d109a95d99617316506a49cbcb0d17
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-10-28 18:02:27 +0200

    FIXED font-awsome not being loaded on Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Add command to allow font-awsome to be loaded as font asset from bower components directory
+Rails.application.config.assets.precompile << Proc.new { |path| path =~ /font-awesome\/fonts/ and File.extname(path).in?(['.otf', '.eot', '.svg', '.ttf', '.woff']) }
