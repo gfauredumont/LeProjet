9ef09bded038f0af66b310679b117479c049130c
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-10-30 17:10:55 +0100

    ADDED mandatory authentication for projects and tasks controller, FORCING project creation with current_user as owner

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
@@ -25,6 +26,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
+    @project.owner = current_user
 
     respond_to do |format|
       if @project.save
