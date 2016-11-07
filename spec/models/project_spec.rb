11b25da255a17b1a61fd1cdadcd6303d7d709e1a
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-07 18:01:58 +0100

    ADDED tests for 'is_public' property and 'published' scope of Project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe Project, :type => :model do
 
   it { should have_many(:tasks) }
 
+
+  it "is not listed in published scope by default" do
+    nb_public_projects = Project.published.all.size
+
+    project = FactoryGirl.create(:project)
+
+    expect(Project.published.all.size).to eq(nb_public_projects)
+  end
+  it "is listed in published scope when made public" do
+    nb_public_projects = Project.published.all.size
+
+    project = FactoryGirl.create(:project)
+
+    project.update(is_public: true)
+    expect(Project.published.all.size).to eq(nb_public_projects + 1)
+  end
+  it "is not listed in published scope when made not public again" do
+    nb_public_projects = Project.published.all.size
+
+    project = FactoryGirl.create(:project)
+
+    project.update(is_public: true)
+    expect(Project.published.all.size).to eq(nb_public_projects + 1)
+
+    project.update(is_public: false)
+    expect(Project.published.all.size).to eq(nb_public_projects)
+  end
+
 end
