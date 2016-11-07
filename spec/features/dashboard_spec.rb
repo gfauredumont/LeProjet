4309e49adfc099ceb820f786d2266ba54867610a
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-07 16:02:55 +0100

    CREATED spec for 'create task' button display in dashboard

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'DashboardDisplay', type: :feature do
+
+  scenario 'When logged out, task creation isn\'t shown' do
+    logout(:user)
+
+    visit root_path
+
+    expect(page).to_not have_submit_button("Create Task")
+  end
+
+  scenario 'When logged in, task creation is shown' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+
+    expect(page).to have_submit_button("Create Task")
+
+    logout(:user)
+  end
+end
