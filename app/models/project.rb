1f42cf89da8794339458bbe122ea175c9baa7f77
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-07 18:03:16 +0100

    CREATED 'published' scope in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,6 +9,8 @@ class Project < ApplicationRecord
 
   belongs_to  :owner, class_name: 'User'
 
+  scope       :published, -> { where(is_public: true) }
+
   def add_member(new_member)
     members << new_member
   end
