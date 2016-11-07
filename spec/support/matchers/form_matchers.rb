61134bb41a0c0cc3c5eaf536b51efaff04329e21
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-07 16:00:20 +0100

    CREATED rspec custom matcher for form elemnts MODIFIED rspec config to load support dir elements

---
+++ spec/support/matchers/form_matchers.rb
@@ -0,0 +1,7 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :have_submit_button do |value|
+  match do |actual|
+    expect(actual).to have_selector("input[type=submit][value='#{value}']")
+  end
+end
