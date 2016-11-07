5d7fce0282e459e83a45737dc1c0840ee78ea2fd
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-07 16:27:05 +0100

    CREATED specs for project creation buttons display in vitrine index depending on user logged in/out status

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -9,7 +9,6 @@ RSpec.feature 'DashboardDisplay', type: :feature do
 
     expect(page).to_not have_submit_button("Create Task")
   end
-
   scenario 'When logged in, task creation is shown' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
@@ -17,6 +16,24 @@ RSpec.feature 'DashboardDisplay', type: :feature do
     visit root_path
 
     expect(page).to have_submit_button("Create Task")
+
+    logout(:user)
+  end
+
+  scenario 'When logged out, project creation buttons aren\'t shown' do
+    logout(:user)
+
+    visit root_path
+
+    expect(page).to_not have_link(nil, href: '/projects/new')
+  end
+  scenario 'When logged in, project creation buttons are shown' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit root_path
+
+    expect(page).to have_link(nil, href: '/projects/new')
 
     logout(:user)
   end
