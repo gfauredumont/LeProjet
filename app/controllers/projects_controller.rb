77c5c6b434d28196917426c91943a45adc9294f3
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-11 19:33:07 +0100

    FIXED project controller not allowing is_published property to be passed as param for update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -71,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description)
+      params.require(:project).permit(:name, :description, :is_public)
     end
 end
