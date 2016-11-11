c6a7e4729ccb50a57642df79492d199692e02e88
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-11-11 19:32:03 +0100

    FIXED vitrine controller fetch projects again (after appController)

--- app/controllers/vitrine_controller.rb
+++ app/controllers/vitrine_controller.rb
@@ -3,7 +3,6 @@ class VitrineController < ApplicationController
   # GET /
   # GET /index.html
   def index
-    @projects = Project.all
     @unassigned_tasks = Task.unassigned
     @new_task = Task.new
   end
